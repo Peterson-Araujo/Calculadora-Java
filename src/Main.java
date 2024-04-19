6f2ea5569309cab61efed34ec806ea1e766326f8
Peterson Gregorio Araujo <petersonaraujo70@gmail.com>
2024-04-19 17:45:29 -0300

    Calculadora v2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,50 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int num1, num2, soma;
-        System.out.print("Digite o primeiro número: ");
-        num1 = sc.nextInt();
-        System.out.print("Digite o segundo número: ");
-        num2 = sc.nextInt();
-        System.out.println();
+        double num1, num2, result;
+        byte operacao;
 
-        soma = num1 + num2;
-        System.out.print("A soma entre " + num1 + " e " + num2 + " é " + soma);
+        System.out.println("Em caso de números quebrados, utilize o ponto (.)");
+        System.out.print("Digite o primeiro número: ");
+        num1 = sc.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        num2 = sc.nextDouble();
+
+        System.out.print("Digite qual o número da operação que deseja realizar \n 1 - Soma \n 2 - Subtração \n 3 - Divisão \n 4 - Multiplicação\n");
+        operacao = sc.nextByte();
+
+        switch (operacao) {
+           case 1:
+               result = num1 + num2;
+               System.out.println("A soma entre " + num1 + " e " + num2 + " é: " + result);
+               break;
+
+           case 2:
+               result = num1 - num2;
+               System.out.println("A subtração entre " + num1 + " e " + num2 + " é: " + result);
+               break;
+
+           case 3:
+               result = num1 / num2;
+               System.out.println("A divisão entre " + num1 + " e " + num2 + " é: " + result);
+               break;
+
+           case 4:
+               result = num1 * num2;
+               System.out.println("A soma multiplicação " + num1 + " e " + num2 + " é: " + result);
+               break;
+
+           default:
+               System.out.println("O tipo de operação não é válido!");
+               break;
+        }
+
+        sc.close();
     }
 }
